fbfed7a1993f7287a7d15710f2463690ac86c5c0
Cbo-11 <c.bolland@live.com>
2020-01-08 20:50:42 +0000

    completed excercise commit

--- main.js
+++ main.js
@@ -1,69 +1,80 @@
-
 var gridArea = document.getElementById("main")
-var tip = 1
+var tip = 0
 grid = 16
 
 creategrid()
 
+
 function changetip() { 
-    if (tip ===1) {
-        tip = 2
-       }
-    else if (tip === 2) {
+    if (tip ===0) {
         tip = 1
        }
+    else if (tip === 1) {
+        tip = 0
+       }
     else return
-}
-
-
-function resizegrid(){
-    var newsize = prompt("New grid length and height?",)
-    grid = newsize
-    document.getElementById('main').innerHTML = ""
-    creategrid()
 }
 
 function creategrid() {
     for( var i = 0; i < grid; i++){
         var row = document.createElement("div");
         row.classList  = "row";
+        row.style.height = `${720/grid}px`
         for( x = 0; x<grid;x++) {
             var cell = document.createElement("div")
             cell.classList = "gridsquare";
+            cell.style.height = `${720/grid}px`
+            cell.style.width = `${720/grid}px`
             row.appendChild(cell)
         }
-        gridArea.appendChild(row)      
+        gridArea.appendChild(row) 
+         
     }
+
+  
+    function hoverChange() {
+        if (tip === 0) {
+        this  .style.background = 'black';;
+        }
+        else if (tip  === 1){
+            var rndcolour = Math.floor(Math.random()*16777215).toString(16);
+            this .style.background = '#'+rndcolour;
+        }
+    }
+
+    function clearChange() {
+        box.forEach( box => { 
+            box.style.background = 'white'
+        })
+    }
+
+
+    function resizegrid(){
+        var newsize = prompt("New grid length and height?",)
+        grid = newsize
+        document.getElementById('main').innerHTML = ""
+        creategrid()
+    }
+
+
+    var clear = document.getElementById("reset")
+    clear.addEventListener("click",clearChange)
+
+    var resize = document.getElementById("resize")
+    resize.addEventListener("click",resizegrid)
+
+    var colourtoggle = document.getElementById("clrchange")
+    colourtoggle.addEventListener("click",changetip)
+
+    var box = document.querySelectorAll('.gridsquare')
+
+    box.forEach( box => {
+        box.addEventListener("mouseover",hoverChange)
+        })
+
+
+
+
 }
 
-var box = document.querySelectorAll('.gridsquare')
 
-box.forEach( box => {
-    box.addEventListener("mouseover",hoverChange)
-    })
-
-function hoverChange() {
-    if (tip === 1) {
-    this  .style.background = 'black';;
-    }
-    else if (tip  === 2){
-        var rndcolour = Math.floor(Math.random()*16777215).toString(16);
-        this .style.background = '#'+rndcolour;
-    }
-  }
-
-function clearChange() {
-    box.forEach( box => { 
-        box.style.background = 'white'
-    })
- }
-
- var clear = document.getElementById("reset")
- clear.addEventListener("click",clearChange)
-
-
- var resize = document.getElementById("resize")
- resize.addEventListener("click",resizegrid)
-
- var colourtoggle = document.getElementById("clrchange")
- colourtoggle.addEventListener("click",changetip)
