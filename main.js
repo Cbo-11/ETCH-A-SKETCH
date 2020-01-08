061875308b66cadb8a44b4c040ae8115e7d15fbf
Cbo-11 <c.bolland@live.com>
2020-01-08 19:11:23 +0000

    commit version 2

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 
 var gridArea = document.getElementById("main")
-var rndcolour = Math.floor(Math.random()*16777215).toString(16);
 var tip = 1
 grid = 16
 
+creategrid()
+
+function changetip() { 
+    if (tip ===1) {
+        tip = 2
+       }
+    else if (tip === 2) {
+        tip = 1
+       }
+    else return
+}
 
 
-for( var i = 0; i <= grid; i++){
-    var row = document.createElement("div");
-    row.classList  = "row";
-    for( x = 1; x<=grid;x++) {
-        var cell = document.createElement("div")
-        cell.classList = "gridsquare";
-        row.appendChild(cell)
+function resizegrid(){
+    var newsize = prompt("New grid length and height?",)
+    grid = newsize
+    document.getElementById('main').innerHTML = ""
+    creategrid()
+}
+
+function creategrid() {
+    for( var i = 0; i < grid; i++){
+        var row = document.createElement("div");
+        row.classList  = "row";
+        for( x = 0; x<grid;x++) {
+            var cell = document.createElement("div")
+            cell.classList = "gridsquare";
+            row.appendChild(cell)
+        }
+        gridArea.appendChild(row)      
     }
-    gridArea.appendChild(row)      
 }
 
 var box = document.querySelectorAll('.gridsquare')
@@ -25,18 +44,26 @@ box.forEach( box => {
 
 function hoverChange() {
     if (tip === 1) {
-    this .classList.add('onhover');
+    this  .style.background = 'black';;
     }
     else if (tip  === 2){
-        this .style.background = rndcolour;
+        var rndcolour = Math.floor(Math.random()*16777215).toString(16);
+        this .style.background = '#'+rndcolour;
     }
   }
 
 function clearChange() {
     box.forEach( box => { 
-        box.classList.remove('onhover')
+        box.style.background = 'white'
     })
  }
 
  var clear = document.getElementById("reset")
  clear.addEventListener("click",clearChange)
+
+
+ var resize = document.getElementById("resize")
+ resize.addEventListener("click",resizegrid)
+
+ var colourtoggle = document.getElementById("clrchange")
+ colourtoggle.addEventListener("click",changetip)
